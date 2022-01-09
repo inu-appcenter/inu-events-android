f3241fbf852d9f4a201574f72ed13521293fdd8d
milk717 <robolindasoo@gmail.com>
2022-01-09 17:23:44 +0900

    chore: 홈 간격 수정

--- app/src/main/java/org/inu/events/HomeViewModel.kt
+++ app/src/main/java/org/inu/events/HomeViewModel.kt
@@ -36,6 +36,7 @@ class HomeViewModel : BaseObservable() {
         homeDataList.add(HomeData("뭐라구?앱센터 13기 신입 멤버를 모집한다구?","마감","동아리",R.drawable.img_home_board_sample_image1,R.drawable.drawable_home_board_date_deadline_background))
         homeDataList.add(HomeData("앱센터 13기 모집","D-12","동아리",R.drawable.img_home_board_sample_image2,R.drawable.drawable_home_board_date_ongoing_background))
         homeDataList.add(HomeData("뭐라구?앱센터가 문제가 아니라 제목이 길어서 문제라고?","D-13","동아리",R.drawable.img_home_board_sample_image1,R.drawable.drawable_home_board_date_ongoing_background))
+        homeDataList.add(HomeData("배고파요","D-17","수달",R.drawable.img_profile,R.drawable.drawable_home_board_date_ongoing_background))
         return homeDataList
     }
 }
