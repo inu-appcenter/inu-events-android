59d0f7002c13337cc585a5bfba8254cf42aed7d6
milk717 <robolindasoo@gmail.com>
2022-01-14 12:13:11 +0900

    refactor: HomeViewModel (BaseObservable -> ViewModel) 변경
    HomeAdapter 코드 부분 수정
    MainActivity 코드 부분 수정
    
    버튼 클릭 리스너 mvvm 패턴으로 변경해야함

--- app/src/main/java/org/inu/events/MainActivity.kt
+++ app/src/main/java/org/inu/events/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import org.inu.events.adapter.HomeAdapter
 import org.inu.events.databinding.ActivityMainBinding
@@ -22,7 +23,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)  //데이터 바인딩
 
-        homeModel = HomeViewModel()     //뷰모델에서 데이터 가져옴
+        homeModel = ViewModelProvider(this).get(HomeViewModel::class.java)    //뷰모델에서 데이터 가져옴
+
         binding.homeRecyclerView.apply {
             layoutManager = GridLayoutManager(context, 2)   //리사이클러뷰 매니저 설정
             adapter = HomeAdapter(homeModel.homeDataList)   //데이터를 아답터에 전달
