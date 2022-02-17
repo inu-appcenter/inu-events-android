bc00ee6df3be1038833e98a28bdfd2401ac8f362
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-02-17 15:04:15 +0900

    addFun: detailActivity 에 initOnOffButton,fetchDataFromSharedPreferences, cancelAlarm 함수 추가

--- app/src/main/java/org/inu/events/DetailActivity.kt
+++ app/src/main/java/org/inu/events/DetailActivity.kt
@@ -1,8 +1,10 @@
 package org.inu.events
 
+import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -20,9 +22,11 @@ import org.inu.events.data.model.dto.AlarmDisplayModel
 import org.inu.events.databinding.ActivityDetailBinding
 import org.inu.events.googlelogin.GoogleLoginWrapper
 import org.inu.events.objects.IntentMessage.EVENT_ID
+import org.inu.events.service.AlarmReceiver
 import org.inu.events.service.LoginService
 import org.inu.events.viewmodel.DetailViewModel
 import org.koin.android.ext.android.inject
+import java.util.*
 
 class DetailActivity : AppCompatActivity() {
     companion object {
@@ -67,12 +71,79 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun initOnOffButton() {
+        observe(viewModel.alarmClickEvent) {
+            val model = binding.onOffButton.tag as? AlarmDisplayModel ?: return@observe
+            val newModel = saveAlarmModel(model.onOff.not())
+            renderOnOffButton(newModel)
+            if (newModel.onOff) {
+                // On -> 알람 등록
+                val calendar = Calendar.getInstance().apply {
+                    // todo 시간 알맞게
+                    val from =  "2022-02-17 02:06:00"
+                    time = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA).parse(from)
+                }
+                val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                val intent = Intent(this, AlarmReceiver::class.java)
+                // todo AlarmReceiver 로 보낼 변수값 putExtra하기
+                val pendingIntent = PendingIntent.getBroadcast(
+                    // 이렇게하면 계속 쌓이기에 ONOFF_KEY 로 하면 각 eventId에 맞게 업데이트
+                    this, id, intent, PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                alarmManager.set(
+                    AlarmManager.RTC_WAKEUP,
+                    calendar.timeInMillis,
+                    pendingIntent
+                )
+            } else {
+                // Off -> 알람 제거
+                cancelAlarm()
+            }
+        }
+    }
+
+    // 각 게시글마다 저장된 onOff fetch
+    private fun fetchDataFromSharedPreferences(): AlarmDisplayModel {
+        val onOffDBValue = sharedPreference.getBoolean(id.toString(),false)
+        val alarmModel = AlarmDisplayModel(
+            onOff = onOffDBValue
+        )
+//         보정 예외처리
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            id,
+            Intent(this, AlarmReceiver::class.java),
+            PendingIntent.FLAG_NO_CREATE
+        )
+
+        if ((pendingIntent == null) and alarmModel.onOff) {
+            // 알람은 꺼져있는데 데이터는 켜져있는 경우
+            alarmModel.onOff = false
+        } else if ((pendingIntent != null) and alarmModel.onOff.not()) {
+            // 알람은 커져있는데 데이터는 꺼져있는 경우
+            // 알람 취소
+            pendingIntent.cancel()
+        }
+        return alarmModel
+    }
+
     private fun saveAlarmModel(onOff: Boolean): AlarmDisplayModel {
         val model = AlarmDisplayModel(
             onOff = onOff
         )
         sharedPreference.getBoolean(id.toString(),model.onOff)
         return model
+    }
+
+    private fun cancelAlarm() {
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            id,
+            Intent(this, AlarmReceiver::class.java),
+            PendingIntent.FLAG_NO_CREATE
+        )
+        pendingIntent?.cancel()
     }
 
     private fun renderOnOffButton(model: AlarmDisplayModel) {
