1b4857f615c81df40e53e0f070e8723c9b353a15
algosketch <sjy_9484@naver.com>
2022-02-18 22:35:23 +0900

    add : 개인정보 처리방침 페이지

--- app/src/main/java/org/inu/events/MyPageActivity.kt
+++ app/src/main/java/org/inu/events/MyPageActivity.kt
@@ -1,11 +1,18 @@
 package org.inu.events
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+    }
+
+    fun onClickPrivacyPolicy(view: View) {
+        val intent = Intent(this, PrivacyPolicyActivity::class.java)
+        startActivity(intent)
     }
 }
