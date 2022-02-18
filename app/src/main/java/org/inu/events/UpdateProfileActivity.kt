c416cafb961bf4bad0a690111517eb9ab329a7bf
algosketch <sjy_9484@naver.com>
2022-02-19 02:06:50 +0900

    add : 닉네임 validation 추가

--- app/src/main/java/org/inu/events/UpdateProfileActivity.kt
+++ app/src/main/java/org/inu/events/UpdateProfileActivity.kt
@@ -1,5 +1,7 @@
 package org.inu.events
 
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.viewModels
 import org.inu.events.base.BaseActivity
 import org.inu.events.databinding.ActivityUpdateProfileBinding
@@ -12,5 +14,15 @@ class UpdateProfileActivity : BaseActivity<ActivityUpdateProfileBinding>() {
     override fun dataBinding() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        binding.nicknameUpdate.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.inputText.value = s.toString()
+                viewModel.validateNickname()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 }
--- app/src/main/java/org/inu/events/viewmodel/UpdateProfileViewModel.kt
+++ app/src/main/java/org/inu/events/viewmodel/UpdateProfileViewModel.kt
@@ -5,4 +5,24 @@ import androidx.lifecycle.ViewModel
 
 class UpdateProfileViewModel : ViewModel() {
     val inputText = MutableLiveData<String>("맥도날드사고싶다")
+    val errorMessage = MutableLiveData<String>("")
+
+    fun validateNickname() : Boolean {
+        errorMessage.value = when {
+            inputText.value!!.isEmpty() -> "공백은 닉네임으로 설정하실 수 없습니다."
+            isDuplicate() -> "중복된 닉네임입니다."
+            !isSuitableLength() -> "닉네임은 8자 이내로 설정해 주세요."
+            else -> ""
+        }
+
+        return errorMessage.value!!.isEmpty()
+    }
+
+    private fun isDuplicate() : Boolean {
+        return false
+    }
+
+    private fun isSuitableLength() : Boolean {
+        return inputText.value!!.length <= 8
+    }
 }
