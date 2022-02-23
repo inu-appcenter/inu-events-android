52667d0021d9b89c1eae38ecff2afab11e4404d6
algosketch <sjy_9484@naver.com>
2022-02-24 07:35:48 +0900

    add : LikeAdapter 추가

---
+++ app/src/main/java/org/inu/events/adapter/LikeAdapter.kt
@@ -0,0 +1,32 @@
+package org.inu.events.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.inu.events.data.model.entity.Event
+import org.inu.events.databinding.ItemFavoriteEventBinding
+
+class LikeAdapter: RecyclerView.Adapter<LikeAdapter.ViewHolder>() {
+    var list: List<Event>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.from(parent)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(list!![position])
+    }
+    override fun getItemCount() = list?.size ?: 0
+
+    class ViewHolder private constructor(val binding: ItemFavoriteEventBinding) : RecyclerView.ViewHolder(binding.root) {
+        companion object {
+            fun from(parent: ViewGroup) : ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemFavoriteEventBinding.inflate(layoutInflater, parent, false)
+
+                return ViewHolder(binding)
+            }
+        }
+
+        fun bind(item: Event) {
+            binding.item = item
+        }
+    }
+}
