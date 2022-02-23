a3aad81185c3506fd8dca54272c2534a16ade1dd
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-02-24 07:25:10 +0900

    chore: DetailActivity.kt notification 서버와 연동을 위한 준비

--- app/src/main/java/org/inu/events/DetailActivity.kt
+++ app/src/main/java/org/inu/events/DetailActivity.kt
@@ -1,37 +1,25 @@
 package org.inu.events
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.icu.text.SimpleDateFormat
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import org.inu.events.MyApplication.Companion.bindImageFromUrl
 import org.inu.events.common.extension.getIntExtra
 import org.inu.events.common.extension.observe
 import org.inu.events.common.extension.observeNonNull
-import org.inu.events.common.extension.toast
-import org.inu.events.data.model.dto.AlarmDisplayModel
 import org.inu.events.databinding.ActivityDetailBinding
 import org.inu.events.dialog.AlarmDialog
 import org.inu.events.dialog.BottomSheetDialog
 import org.inu.events.googlelogin.GoogleLoginWrapper
-import org.inu.events.objects.IntentMessage.BACK_FROM_ALARM
 import org.inu.events.objects.IntentMessage.EVENT_ID
-import org.inu.events.service.AlarmReceiver
 import org.inu.events.service.LoginService
 import org.inu.events.viewmodel.DetailViewModel
 import org.koin.android.ext.android.inject
-import java.util.*
 
 class DetailActivity : AppCompatActivity() {
     companion object {
@@ -39,8 +27,6 @@ class DetailActivity : AppCompatActivity() {
             Intent(context, DetailActivity::class.java).apply {
                 putExtra(EVENT_ID, eventId)
             }
-
-        private const val SHARED_PREFERENCES_NAME = "alarm"
     }
 
     // 전역 변수로 변경
@@ -51,7 +37,6 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
 
     private val googleLogin = GoogleLoginWrapper(this)
-    private var backFromAlarm = false
 
     private val bottomDialog = BottomSheetDialog()
     private val alarmDialog = AlarmDialog()
@@ -62,7 +47,7 @@ class DetailActivity : AppCompatActivity() {
         initBinding()
 
         initCommentButton()
-        initOnOffButton()
+//        initNotificationButton()
 
         setupToolbar()
 
@@ -81,93 +66,34 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-    // onOffButton clickEvent
-    private fun initOnOffButton() {
+    private fun initNotificationButton() {
         observe(viewModel.alarmClickEvent) {
-            val model = binding.onOffButton.tag as? AlarmDisplayModel ?: return@observe
-            val newModel = saveAlarmModel(model.onOff.not())
+            if (viewModel.onOff.value == false) {
+                bottomDialog.show(
+                    this,
+                    {
+                        viewModel.postNotification("start")
+                        alarmDialog.showDialog(this, resources.getString(R.string.alarm_on_title_start), resources.getString(R.string.alarm_on_content_start))
 
-            if (newModel.onOff){
-                bottomDialog.show(this, {
-                    registerAlarm(true)
-                    renderOnOffButton(newModel)
-                }, {
-                    registerAlarm(false)
-                    renderOnOffButton(newModel)
-                }, {
-                    return@show
-                })
+                    },
+                    {
+                        viewModel.postNotification("end")
+                        alarmDialog.showDialog(this, resources.getString(R.string.alarm_on_title_last), resources.getString(R.string.alarm_on_content_last))
+                    },
+                    {
+                        // 취소 클릭
+                    })
             }
             else{
-                alarmDialog.showDialog(this, title = getString(R.string.alarm_off_title), content =getString(R.string.alarm_off_content))
-                AlarmForm(this, eventID = id).cancelAlarm()
-                renderOnOffButton(newModel)
+                viewModel.deleteNotification()
+                alarmDialog.showDialog(this,resources.getString(R.string.alarm_off_title),resources.getString(R.string.alarm_off_content))
             }
         }
-    }
-
-    // 알람 등록 및 취소
-    private fun registerAlarm(isBeforeStart:Boolean){
-        if (isBeforeStart){
-            alarmDialog.showDialog(this, title = getString(R.string.alarm_on_title_start), content = getString(R.string.alarm_on_content_start))
-        }
-        else{
-            alarmDialog.showDialog(this, title = getString(R.string.alarm_on_title_last), content = getString(R.string.alarm_on_content_last))
-        }
-        // On -> 알람 등록
-        AlarmForm(this, eventID = id).registerAlarmOnDetail(
-            title = binding.detailTitle.text.toString(), date = "2022-02-23 21:05:00", isBeforeStart = isBeforeStart)
-    }
-
-    // 알람 버튼 요소 변경
-    private fun renderOnOffButton(model: AlarmDisplayModel) {
-        viewModel.loadOnOffButton(onOff = model.onOff)
-        binding.onOffButton.tag = model
-    }
-
-    // 각 게시글마다 저장된 onOff fetch
-    private fun fetchDataFromSharedPreferences(): AlarmDisplayModel {
-        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
-        val onOffDBValue = sharedPreferences.getBoolean(id.toString(), false)
-        val alarmModel = AlarmDisplayModel(
-            onOff = onOffDBValue
-        )
-//         보정 예외처리
-        val pendingIntent = PendingIntent.getBroadcast(
-            this,
-            id,
-            Intent(this, AlarmReceiver::class.java),
-            PendingIntent.FLAG_NO_CREATE
-        )
-
-        if ((pendingIntent == null) and alarmModel.onOff) {
-            // 알람은 꺼져있는데 데이터는 켜져있는 경우
-            alarmModel.onOff = false
-        } else if ((pendingIntent != null) and alarmModel.onOff.not()) {
-            // 알람은 커져있는데 데이터는 꺼져있는 경우
-            // 알람 취소
-            pendingIntent.cancel()
-        }
-        return alarmModel
-    }
-
-    // 알람버튼 클릭될때마다 onOff 저장
-    private fun saveAlarmModel(onOff: Boolean): AlarmDisplayModel {
-        val model = AlarmDisplayModel(
-            onOff = onOff
-        )
-        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
-        with(sharedPreferences.edit()) {
-            putBoolean(id.toString(), model.onOff)
-            commit()
-        }
-
-        return model
     }
 
 
     private fun setupToolbar() {
-        binding.detailToolbar.toolbarImageView.setOnClickListener { isFromAlarm() }
+        binding.detailToolbar.toolbarImageView.setOnClickListener { finish() }
         //todo - 툴바메뉴는 자신이 작성한 글일 경우에만 노출돼야함
         if (loginService.isLoggedIn) {
             if (viewModel.isMyWriting()) {
@@ -211,31 +137,11 @@ class DetailActivity : AppCompatActivity() {
         super.onStart()
 
         extractEventIdAndLoad()
-
-        val model = fetchDataFromSharedPreferences()
-        renderOnOffButton(model)
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-
-        isFromAlarm()
     }
 
     private fun extractEventIdAndLoad() {
         id = getIntExtra(EVENT_ID) ?: return
         viewModel.load(id)
-    }
-
-    // 이 액티비티가 알람에서 왔다면 뒤로가기 처리를 해주세요~
-    private fun isFromAlarm(){
-        backFromAlarm = intent.getBooleanExtra(BACK_FROM_ALARM,false)
-        if (backFromAlarm) {
-            val intent = Intent(this,MainActivity::class.java)
-//                flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-        }
-        finish()
     }
 
 }
