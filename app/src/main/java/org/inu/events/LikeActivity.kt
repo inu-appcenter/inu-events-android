824edf140465750529a8ca7dc6f805ceb01505e9
algosketch <sjy_9484@naver.com>
2022-02-24 07:17:38 +0900

    add : 저장 목록 액티비티 추가

---
+++ app/src/main/java/org/inu/events/LikeActivity.kt
@@ -0,0 +1,10 @@
+package org.inu.events
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.inu.events.base.BaseActivity
+import org.inu.events.databinding.ActivityLikeBinding
+
+class LikeActivity : BaseActivity<ActivityLikeBinding>() {
+    override val layoutResourceId = R.layout.activity_like
+}
---
+++ app/src/main/java/org/inu/events/viewmodel/LikeViewModel.kt
@@ -0,0 +1,6 @@
+package org.inu.events.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class LikeViewModel : ViewModel() {
+}
