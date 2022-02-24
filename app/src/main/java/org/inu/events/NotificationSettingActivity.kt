1aec3d63d54c0544722755db526cc9db085719c5
algosketch <sjy_9484@naver.com>
2022-02-25 03:01:10 +0900

    add : 새로운 행사 알림 설정 페이지

---
+++ app/src/main/java/org/inu/events/NotificationSettingActivity.kt
@@ -0,0 +1,10 @@
+package org.inu.events
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.inu.events.base.BaseActivity
+import org.inu.events.databinding.ActivityNotificationSettingBinding
+
+class NotificationSettingActivity : BaseActivity<ActivityNotificationSettingBinding>() {
+    override val layoutResourceId = R.layout.activity_notification_setting
+}
---
+++ app/src/main/java/org/inu/events/viewmodel/NotificationSettingViewModel.kt
@@ -0,0 +1,6 @@
+package org.inu.events.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class NotificationSettingViewModel : ViewModel() {
+}
