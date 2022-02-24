33d55dd3b1a600347e6d8a5a0727df0c4cb79909
algosketch <sjy_9484@naver.com>
2022-02-25 06:48:26 +0900

    add : 사람 아이콘 누르면 로그인 다이어로그

--- app/src/main/java/org/inu/events/viewmodel/HomeViewModel.kt
+++ app/src/main/java/org/inu/events/viewmodel/HomeViewModel.kt
@@ -5,18 +5,22 @@ import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.inu.events.LoginActivity
 import org.inu.events.MyPageActivity
 import org.inu.events.ToolbarListener
 import org.inu.events.common.threading.execute
 import org.inu.events.common.util.SingleLiveEvent
 import org.inu.events.data.model.entity.Event
 import org.inu.events.data.repository.EventRepository
+import org.inu.events.dialog.LoginDialog
+import org.inu.events.service.LoginService
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class HomeViewModel : ViewModel(), KoinComponent {
 
     private val eventRepository: EventRepository by inject()
+    private val loginService: LoginService by inject()
 
     private val _homeDataList = MutableLiveData<List<Event>>()
     val homeDataList: LiveData<List<Event>>
@@ -25,8 +29,16 @@ class HomeViewModel : ViewModel(), KoinComponent {
     val postClickEvent = SingleLiveEvent<Any>()
     val toolbarListener = object : ToolbarListener {
         override fun onClickMyPage(view: View) {
-            val intent = Intent(view.context, MyPageActivity::class.java)
-            view.context.startActivity(intent)
+            if(loginService.isLoggedIn) {
+                val intent = Intent(view.context, MyPageActivity::class.java)
+                view.context.startActivity(intent)
+            }
+            else {
+                val intent = Intent(view.context, LoginActivity::class.java)
+                LoginDialog().show(view.context,
+                    { view.context.startActivity(intent) },
+                    { } )
+            }
         }
     }
 
