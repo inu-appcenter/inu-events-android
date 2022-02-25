433eb4925056de35a9e11dd936c2ae8d46cdc125
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-02-26 04:47:04 +0900

    fix: Like 요청 변수 json 으로 보내기 위해 감싸기

--- app/src/main/java/org/inu/events/data/httpservice/LikeHttpService.kt
+++ app/src/main/java/org/inu/events/data/httpservice/LikeHttpService.kt
@@ -1,5 +1,6 @@
 package org.inu.events.data.httpservice
 
+import org.inu.events.data.model.dto.LikeParam
 import org.inu.events.data.model.entity.Event
 import retrofit2.Call
 import retrofit2.http.Body
@@ -9,10 +10,10 @@ import retrofit2.http.POST
 
 interface LikeHttpService {
     @POST("/likes")
-    fun postLike(@Body eventId : Int) : Call<Unit>
+    fun postLike(@Body param: LikeParam) : Call<Unit>
 
     @DELETE("/likes")
-    fun deleteLike(@Body eventId: Int) : Call<Unit>
+    fun deleteLike(@Body param:LikeParam) : Call<Unit>
 
     @GET("/likes")
     fun getLikes(): Call<List<Event>>
