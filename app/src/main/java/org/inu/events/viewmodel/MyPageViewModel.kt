a4b12df824468aabcf688a1d9714003c8cfd248c
algosketch <sjy_9484@naver.com>
2022-02-26 03:05:28 +0900

    add : toolbar 에 백 버튼 listener 추가

--- app/src/main/java/org/inu/events/viewmodel/MyPageViewModel.kt
+++ app/src/main/java/org/inu/events/viewmodel/MyPageViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.inu.events.*
+import org.inu.events.common.util.SingleLiveEvent
 import org.inu.events.data.model.entity.User
 import org.inu.events.service.UserService
 import org.koin.core.component.KoinComponent
@@ -17,6 +18,13 @@ import org.koin.core.component.inject
 class MyPageViewModel : ViewModel(), KoinComponent {
     private val userService: UserService by inject()
     val user = MutableLiveData<User>()
+    val onClickBackEvent = SingleLiveEvent<Any>()
+
+    val backButtonListener = object : BackButtonListener {
+        override fun invoke(view: View) {
+            onClickBackEvent.call()
+        }
+    }
 
     fun fetchData() {
         CoroutineScope(Dispatchers.Main).launch {
