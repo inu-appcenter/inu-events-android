7dce350de599fd71b29aeeb887c083f857de72f1
algosketch <sjy_9484@naver.com>
2022-02-26 22:18:58 +0900

    add : 저장 목록 추가

--- app/src/main/java/org/inu/events/adapter/LikeAdapter.kt
+++ app/src/main/java/org/inu/events/adapter/LikeAdapter.kt
@@ -2,35 +2,50 @@ package org.inu.events.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import org.inu.events.data.model.entity.Event
-import org.inu.events.databinding.ItemFavoriteEventBinding
+import org.inu.events.databinding.ItemLikeEventBinding
+import org.inu.events.viewmodel.LikeViewModel
 
-class LikeAdapter: RecyclerView.Adapter<LikeAdapter.ViewHolder>() {
-    private var list: List<Event>? = null
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.from(parent)
+class LikeAdapter(val viewModel: LikeViewModel) : ListAdapter<Event, LikeAdapter.ViewHolder>(LikeDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.from(parent, viewModel)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(list!![position])
-    }
-    override fun getItemCount() = list?.size ?: 0
-
-    fun submitList(list: List<Event>) {
-        this.list = list
+        holder.bind(getItem(position))
     }
 
-    class ViewHolder private constructor(val binding: ItemFavoriteEventBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder private constructor(val binding: ItemLikeEventBinding, val viewModel: LikeViewModel) : RecyclerView.ViewHolder(binding.root) {
         companion object {
-            fun from(parent: ViewGroup) : ViewHolder {
+            fun from(parent: ViewGroup, viewModel: LikeViewModel) : ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = ItemFavoriteEventBinding.inflate(layoutInflater, parent, false)
+                val binding = ItemLikeEventBinding.inflate(layoutInflater, parent, false)
 
-                return ViewHolder(binding)
+                return ViewHolder(binding, viewModel)
             }
         }
 
         fun bind(item: Event) {
             binding.item = item
+            binding.viewModel = viewModel
+            binding.bookmarkIcon.setOnClickListener {
+                viewModel.onClickLikeIcon(item.id)
+            }
+            binding.cardWrap.setOnClickListener {
+                viewModel.onClickDetail(it, item)
+            }
+            binding
+            binding.executePendingBindings()
         }
+    }
+}
+
+class LikeDiffUtil : DiffUtil.ItemCallback<Event>() {
+    override fun areItemsTheSame(oldItem: Event, newItem: Event): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Event, newItem: Event): Boolean {
+        return oldItem == newItem
     }
 }
--- app/src/main/java/org/inu/events/viewmodel/LikeViewModel.kt
+++ app/src/main/java/org/inu/events/viewmodel/LikeViewModel.kt
@@ -1,14 +1,43 @@
 package org.inu.events.viewmodel
 
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.inu.events.DetailActivity
+import org.inu.events.data.model.dto.LikeParam
 import org.inu.events.data.model.entity.Event
+import org.inu.events.data.repository.LikeRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class LikeViewModel : ViewModel() {
-    val eventList = listOf(
-            Event(
-                1, 1, "123", "234", "324", "1241423", "", "sdf",
-                "sdf", "sdf", "sdf", "", "", "", "", false,
-                false, false, "", 5, 11, 6, 1, ""
-            )
-        )
+class LikeViewModel : ViewModel(), KoinComponent {
+    private val likeRepository: LikeRepository by inject()
+
+    val eventList = MutableLiveData<List<Event>>()
+
+    fun loadLikes() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val eventsDeferred = async(Dispatchers.IO) { likeRepository.getLikes() }
+            val eventList = eventsDeferred.await()
+            this@LikeViewModel.eventList.postValue(eventList)
+        }
+    }
+
+    fun onClickLikeIcon(eventId: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val likeParam = LikeParam(eventId = eventId)
+            likeRepository.deleteLike(likeParam)
+            loadLikes()
+        }
+    }
+
+    fun onClickDetail(view: View, event: Event) {
+        with(view.context) {
+            startActivity(DetailActivity.callingIntent(this, event.id, event.wroteByMe))
+        }
+    }
 }
