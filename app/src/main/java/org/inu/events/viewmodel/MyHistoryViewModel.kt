00d27606c81aed844cebc91e6f88acf8d75253b4
algosketch <sjy_9484@naver.com>
2022-02-28 05:43:46 +0900

    refactor : 기존 툴바 제거 (toolbar_with_back_button)

--- app/src/main/java/org/inu/events/viewmodel/MyHistoryViewModel.kt
+++ app/src/main/java/org/inu/events/viewmodel/MyHistoryViewModel.kt
@@ -1,10 +1,7 @@
 package org.inu.events.viewmodel
 
-import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import org.inu.events.BackButtonListener
-import org.inu.events.common.util.SingleLiveEvent
 
 class MyHistoryViewModel : ViewModel() {
     val title = MutableLiveData("내가 쓴 글")
