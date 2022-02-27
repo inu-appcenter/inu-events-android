f7429757f628f6a1d5f3d58784fa44abfd9f4be4
algosketch <sjy_9484@naver.com>
2022-02-28 07:44:32 +0900

    add : 각각의 item 들에 이미지 적용

--- app/src/main/java/org/inu/events/MyApplication.kt
+++ app/src/main/java/org/inu/events/MyApplication.kt
@@ -1,15 +1,18 @@
 package org.inu.events
 
 import android.app.Application
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import org.inu.events.di.myModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import java.util.*
 
 
 class MyApplication : Application() {
@@ -30,11 +33,26 @@ class MyApplication : Application() {
             }
         }
 
+
+        @BindingAdapter("bindImageRadius4")
+        @JvmStatic
+        fun bindImage(view: ImageView, imageUrl: String?) {
+            val displayMetrics = view.context.resources.displayMetrics
+            val radiusWithPixel = (displayMetrics.density * 4).toInt()
+
+            Glide.with(view.context)
+                .load(imageUrl)
+                .transform(CenterCrop(), RoundedCorners(radiusWithPixel))
+                .error(R.drawable.img_default)
+                .placeholder(R.drawable.img_default)
+                .into(view)
+        }
+
         @BindingAdapter("app:photo_url")
         @JvmStatic
-        fun setPhoto(view: ImageView, iamgeUrl: String?) {
+        fun setPhoto(view: ImageView, imageUrl: String?) {
             Glide.with(view.context)
-                .load(iamgeUrl)
+                .load(imageUrl)
                 .error(R.drawable.default_profile_background)
                 .placeholder(R.drawable.default_profile_background)
                 .into(view)
