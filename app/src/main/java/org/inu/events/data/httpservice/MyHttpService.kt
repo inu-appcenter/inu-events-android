9f69752ee73e91f5ea141612f69511931515a069
algosketch <sjy_9484@naver.com>
2022-02-28 06:30:04 +0900

    add : 내 글, 댓글 관련 api interface 구현

---
+++ app/src/main/java/org/inu/events/data/httpservice/MyHttpService.kt
@@ -0,0 +1,13 @@
+package org.inu.events.data.httpservice
+
+import org.inu.events.data.model.entity.Event
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MyHttpService {
+    @GET("/mycomments")
+    fun getComments(): Call<List<Event>>
+
+    @GET("/myevents")
+    fun getEvents(): Call<List<Event>>
+}
