dbcd217b8f509eb80f6bbf5b9237e3c7aa915b23
algosketch <sjy_9484@naver.com>
2022-03-06 08:54:42 +0900

    refactor : getter 사용하도록 변경

--- app/src/main/java/org/inu/events/common/util/Period.kt
+++ app/src/main/java/org/inu/events/common/util/Period.kt
@@ -13,22 +13,30 @@ class Period {
     val endDate = MutableLiveData("")
     val endTime = MutableLiveData("")
 
-    fun serverDateToString(date: String): String{
+    val formattedStartDateTime: String
+        get() = datePickerToString(startDate.value!!, startTime.value!!)
+    val formattedEndDateTime: String
+        get() = datePickerToString(endDate.value!!, endTime.value!!)
+
+    fun serverDateToString(date: String): String {
         val stringDate: LocalDate = LocalDate.parse(date, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
         return stringDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
     }
 
-    fun serverTimeToString(time: String): String{
+    fun serverTimeToString(time: String): String {
         val timeDate: LocalTime = LocalTime.parse(time, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
         return timeDate.format(DateTimeFormatter.ofPattern("hh:mm a", Locale("en", "KO")))
     }
 
-    private fun formatDateForServer(date: String): String{
-        val serverDate = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm a", Locale("en", "KO")))
+    private fun formatDateForServer(date: String): String {
+        val serverDate = LocalDateTime.parse(
+            date,
+            DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm a", Locale("en", "KO"))
+        )
         return serverDate.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
     }
 
-    fun datePickerToString(startDatePeriod: String, startTimePeriod: String): String {
-        return formatDateForServer("$startDatePeriod $startTimePeriod")
+    fun datePickerToString(date: String, time: String): String {
+        return formatDateForServer("$date $time")
     }
 }
