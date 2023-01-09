80cc2b5043d015f64d507b35eaac9af73b0fbd68
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-01-09 22:31:26 +0900

    feat: 검색 구현! (카테고리 및 상태 제외)

--- app/src/main/java/org/inu/events/ui/home/SearchActivity.kt
+++ app/src/main/java/org/inu/events/ui/home/SearchActivity.kt
@@ -3,17 +3,45 @@ package org.inu.events.ui.home
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.inu.events.databinding.ActivitySearchBinding
+import org.inu.events.ui.adapter.like.LikePagingAdapter
 
 class SearchActivity : AppCompatActivity() {
 
     private val vm: SearchViewModel by viewModels()
     private lateinit var binding: ActivitySearchBinding
+    private val adapter = LikePagingAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater).apply {
+            lifecycleOwner = this@SearchActivity
             vm = this@SearchActivity.vm
+        }
+
+        initRecyclerView()
+        observeSearch()
+
+        setContentView(binding.root)
+    }
+
+
+    private fun initRecyclerView() {
+        binding.rvEvent.adapter = adapter
+    }
+
+    private fun observeSearch() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                vm.searchResult.collectLatest {
+                    adapter.submitData(it)
+                }
+            }
         }
     }
 }
--- app/src/main/java/org/inu/events/ui/home/SearchViewModel.kt
+++ app/src/main/java/org/inu/events/ui/home/SearchViewModel.kt
@@ -1,6 +1,59 @@
 package org.inu.events.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import org.inu.events.data.model.entity.Event
+import org.inu.events.data.repository.EventRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class SearchViewModel: ViewModel() {
+class SearchViewModel : ViewModel(), KoinComponent {
+
+    private val eventRepository: EventRepository by inject()
+
+    val searchText = MutableStateFlow("")
+    private val category = MutableStateFlow(0)
+    private val eventStatus = MutableStateFlow(false)
+
+    var searchResult: MutableStateFlow<PagingData<Event>> = MutableStateFlow(PagingData.empty())
+    private var job: Job? = null
+
+    init {
+
+        viewModelScope.launch {
+            initSearch()
+        }
+
+    }
+
+    @OptIn(FlowPreview::class)
+    private suspend fun initSearch() {
+        searchText.debounce(500).collect {
+            job?.cancel()
+            if (it.isEmpty()) {
+                searchResult.value = PagingData.empty()
+                return@collect
+            }
+            search().run {
+                job = this
+            }
+        }
+    }
+
+    private fun search(): Job {
+        return viewModelScope.launch {
+            eventRepository.searchEvents(
+                categoryId = category.value,
+                eventStatus = eventStatus.value,
+                content = searchText.value
+            ).collectLatest { pagingData ->
+                searchResult.value = pagingData
+            }
+        }
+    }
 }
